09e1f76f6136615e8c202f4a7f0f2e64cbd356b1
alchemist7991 <sauravdrulez@gmail.com>
2023-12-24 23:51:54 +0530

    refactor: Readme docs

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Start Services
+`docker-compose up`
+
+## Stop Services
+`docker-compose down -v`
+
+## SSH Into Container
+`docker exec -it <continaer Id> bash`
--- app_mongo/index.js
+++ app_mongo/index.js
@@ -7,7 +7,6 @@ app.use(express.json());
 
 
 const setupDone = async () => {
-  console.log(process.env.MONGO_URL)
   mongoose.connect(process.env.MONGO_URL);
   const User = new mongoose.model('user', { 
     name: String,
@@ -16,10 +15,9 @@ const setupDone = async () => {
   });
   const client = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_BOOTSTRAP_PORT });
   const consumer = new kafka.Consumer(client, [{topic: process.env.KAFKA_TOPIC}], {autoCommit: false});
-  consumer.on('message', async(message) => {
+  consumer.on('message', async(message) => { 
     console.log('New message received' + JSON.stringify(message.value));
     const newUser = await new User(JSON.parse(message.value));
-    console.log("======" + newUser);
     await newUser.save();
   });
 
@@ -33,3 +31,6 @@ setTimeout(setupDone, 2000);
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
+// Once you boot up all services with docker. There are no existing Topics to read from.
+// Thus you can hit an api such that it will create the topic first and write some data into kafka
+// Then you can make a small change in the code, since we have volume mounted and there is nodeMon so it will auto restart all the services
